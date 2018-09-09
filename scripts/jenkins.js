f3fa36f7da095f287e020fdc9f568674f96d118d
Seaborn Lee <seabornlee.cn@gmail.com>
2018-09-09 11:34:32 +0800

    feat: 去除前缀

--- scripts/jenkins.js
+++ scripts/jenkins.js
@@ -1,11 +1,9 @@
 const JENKINS_URL = process.env.JENKINS_URL;
-const JENKINS_JOB_PREFIX = process.env.JENKINS_JOB_PREFIX;
-const JENKINS_JOB_TOKEN = process.env.JENKINS_JOB_TOKEN;
 module.exports = (robot) => {
   robot.respond(/build (.*)/i, (res) => {
-    const jobName = JENKINS_JOB_PREFIX + res.match[1]
+    const jobName = res.match[1]
 
-    const url = JENKINS_URL + "/buildByToken/build?job=" + jobName + "&token=" + JENKINS_JOB_TOKEN;
+    const url = JENKINS_URL + "/buildByToken/build?job=" + jobName + "&token=" + jobName;
     robot.http(url).get()((_err, _res, _body) => {
       robot.logger.debug(_body);
       res.reply("Ok, Start building...");
