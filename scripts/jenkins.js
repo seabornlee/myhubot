75d5b81bd6bdb641c4b463e820da5ce1e00b1295
Seaborn Lee <seabornlee.cn@gmail.com>
2018-09-09 10:57:44 +0800

    feat: 构建任何 Job

--- scripts/jenkins.js
+++ scripts/jenkins.js
@@ -1,9 +1,11 @@
 const JENKINS_URL = process.env.JENKINS_URL;
-const JENKINS_JOB_NAME = process.env.JENKINS_JOB_NAME;
+const JENKINS_JOB_PREFIX = process.env.JENKINS_JOB_PREFIX;
 const JENKINS_JOB_TOKEN = process.env.JENKINS_JOB_TOKEN;
 module.exports = (robot) => {
-  robot.respond(/jenkins:build/i, (res) => {
-    const url = JENKINS_URL + "/buildByToken/build?job=" + JENKINS_JOB_NAME + "&token=" + JENKINS_JOB_TOKEN;
+  robot.respond(/build (.*)/i, (res) => {
+    const jobName = JENKINS_JOB_PREFIX + res.match[1]
+
+    const url = JENKINS_URL + "/buildByToken/build?job=" + jobName + "&token=" + JENKINS_JOB_TOKEN;
     robot.http(url).get()((_err, _res, _body) => {
       robot.logger.debug(_body);
       res.reply("Ok, Start building...");
