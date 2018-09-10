f77d19a2ce5cf9c69f7b1183a4972eaaaacc82b2
Seaborn Lee <seabornlee.cn@gmail.com>
2018-09-10 23:29:31 +0800

    feat: 构建触发失败时给出相应提示

--- scripts/jenkins.js
+++ scripts/jenkins.js
@@ -5,8 +5,12 @@ module.exports = (robot) => {
 
     const url = JENKINS_URL + "/buildByToken/build?job=" + jobName + "&token=" + jobName;
     robot.http(url).get()((_err, _res, _body) => {
+      if (_res.statusCode === 201) {
+        res.reply("Ok, Start building...");
+      } else {
+        res.reply("皇上，臣妾做不到啊~");
+      }
       robot.logger.debug(_body);
-      res.reply("Ok, Start building...");
     });
   });
 };
